0ae624c4e85098a8c83011cbfbe1fd1abd0538d5
shanellobo <shanelllobo06@gmail.com>
2025-08-07 19:55:33 +0400

    Add files via upload

---
+++ ReverseOfNumber/ReverseOfNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ReverseOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find the reverse:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Reverse r = new Reverse(num);
+            r.process();
+            r.display();
+        }
+    }
+}
---
+++ ReverseOfNumber/ReverseOfNumber/Reverse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseOfNumber
+{
+    public class Reverse
+    {
+        int num;
+        int rev = 0;
+        int temp;
+        public Reverse(int num)
+        {
+            this.num = num;
+        }
+        public void process()
+        {
+            while (num > 0)
+            {
+                temp = num % 10;
+                num /= 10;
+                rev = rev * 10 + temp;
+            }
+
+        }
+        public void display()
+        {
+            Console.WriteLine($"The Reversed Number is {rev}");
+        }
+    }
+}
+
+
