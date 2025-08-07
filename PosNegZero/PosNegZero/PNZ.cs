dadaea632c44c055549a8934ef67294be4558b9d
shanellobo <shanelllobo06@gmail.com>
2025-08-07 19:50:11 +0400

    PosNegZero

---
+++ PosNegZero/PosNegZero/PNZ.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosNegZero
+{
+    public class PNZ
+    {
+        int num;
+
+        public PNZ()
+        {
+            num = 2;
+
+        }
+        public PNZ(int num)
+        {
+            this.num = num;
+        }
+        public void check()
+        {
+            if (num > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+            else
+            {
+                Console.WriteLine("Zero");
+            }
+        }
+    }
+}
---
+++ PosNegZero/PosNegZero/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PosNegZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            PNZ pnz = new PNZ(num);
+            pnz.check();
+        }
+    }
+}
