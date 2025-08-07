b6a2e7e7e03d13d7f914d87fccfdd3c56744385a
shanellobo <shanelllobo06@gmail.com>
2025-08-07 19:44:55 +0400

    Greet

---
+++ greet/greet/Greet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greet
+{
+    public class Greet
+    {
+        string name;
+        public Greet()
+        {
+            name = "John Doe";
+        }
+        public Greet(string name)
+        {
+            this.name = name;
+        }
+        public void greet()
+        {
+            Console.WriteLine($"Good Morning, {name}!");
+        }
+    }
+}
---
+++ greet/greet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using greet;
+
+namespace GreetPerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name:");
+            String name = Console.ReadLine();
+            Greet g = new Greet(name);
+            g.greet();
+        }
+    }
+}
