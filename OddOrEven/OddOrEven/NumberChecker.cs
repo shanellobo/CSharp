a3353261fdb24f5010def16572655ca05a9f1054
shanellobo <shanelllobo06@gmail.com>
2025-08-08 12:22:04 +0530

    Add files via upload

---
+++ OddOrEven/OddOrEven/NumberChecker.cs
@@ -0,0 +1,11 @@
+﻿namespace OddEvenChecker
+{
+    public class NumberChecker
+    {
+        // Method to check if the number is even
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
---
+++ OddOrEven/OddOrEven/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OddEvenChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                NumberChecker checker = new NumberChecker();
+                if (checker.IsEven(number))
+                {
+                    Console.WriteLine($"{number} is even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
+
