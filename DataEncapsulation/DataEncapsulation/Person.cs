201c99f16b8d176ae2b424d87a0eed8a7ebd1daa
shanellobo <shanelllobo06@gmail.com>
2025-08-08 12:04:28 +0530

    Add files via upload

---
+++ DataEncapsulation/DataEncapsulation/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEncapsulation
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0 && value < 100)
+                    age = value;
+                else
+                    Console.WriteLine("age must be between 0 and 100");
+          
+            }
+        }
+    }
+}
+
---
+++ DataEncapsulation/DataEncapsulation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DataEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "alice";
+            person.Age = 25;
+            Console.WriteLine($"the name is {person.Name} and the age is {person.Age}");
+        }
+    }
+}
