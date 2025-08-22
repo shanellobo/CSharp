826b5b8de5c297cb0664a07696ba34f071c6578d
shanellobo <shanelllobo06@gmail.com>
2025-08-22 12:20:00 +0530

    VehicleRental

---
+++ VehicleRentalService/VehicleRentalService/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleRentalService;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle v = new Vehicle("Bentley", "Bentagya", 300);
+        v.display();
+        Vehicle c = new Car("ZRV", "Honda", 699, 5, "Petrol");
+        c.display();
+    }
+}
