8adb4252ca100255a3f0df14c9107c3d4c2ca5e0
shanellobo <shanelllobo06@gmail.com>
2025-08-22 12:05:55 +0530

    Librarymanagement

---
+++ LibraryManagement/LibraryManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books b = new Books("Tinkle", "Anant Pai");
+            b.DisplayDetails();
+            Books e = new EBook("Atomic Habits", "James Clear", 100, "ATH");
+            e.DisplayDetails();
+        }
+    }
+}
