5b2d1b105cc53bb54b22f011fbe59290a97150c8
shanellobo <shanelllobo06@gmail.com>
2025-10-03 12:33:22 +0530

    Store

---
+++ Store/Store/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Store
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Price: {Price}");
+        }
+    }
+
+    public class EProduct : Product
+    {
+        public string Brand { get; set; }
+        public int Warranty { get; set; }
+
+        public EProduct(string name, double price, string brand, int warranty)
+            : base(name, price)
+        {
+            Brand = brand;
+            Warranty = warranty;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Price: {Price}, Brand: {Brand}, Warranty: {Warranty} years");
+        }
+    }
+
+    public class Smartphone : EProduct
+    {
+        public string OperatingSystem { get; set; }
+
+        public Smartphone(string name, double price, string brand, int warranty, string operatingSystem)
+            : base(name, price, brand, warranty)
+        {
+            OperatingSystem = operatingSystem;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Operating System: {OperatingSystem}");
+        }
+    }
+}
---
+++ Store/Store/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("Pillow", 99.99);
+            p.Display();
+            EProduct e = new EProduct("Fridge", 499.99, "Haier", 5);
+            e.Display();
+            Smartphone s = new Smartphone("Smartphone", 50000, "iPhone", 1, "iOS");
+            s.Display();
+        }
+    }
+}
