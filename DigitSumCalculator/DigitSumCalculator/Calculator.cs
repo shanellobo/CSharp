e4bc61ae99c493f7e4abd3b589b749ca08e4c1e2
shanellobo <shanelllobo06@gmail.com>
2025-10-03 12:29:51 +0530

    DigitalSumCalculator.cs

---
+++ DigitSumCalculator/DigitSumCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Calculator
+{
+    private int number;
+
+    public void ReadNumber()
+    {
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void CalculateAndDisplaySum()
+    {
+        int sum = 0;
+        int temp = Math.Abs(number);
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+
+        Console.WriteLine($"Sum of digits of {number} is: {sum}");
+    }
+}
+
---
+++ DigitSumCalculator/DigitSumCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        calculator.ReadNumber();
+        calculator.CalculateAndDisplaySum();
+
+        Console.ReadKey();
+    }
+}
